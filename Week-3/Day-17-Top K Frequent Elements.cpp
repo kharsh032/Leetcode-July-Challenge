228ce7c0f67f80fa55793e9b706e8f8ce0cb039e
Harsh <kharsh032@gmail.com>
2020-07-17 14:03:55 +0530

    Top K Frequent Elements

---
+++ Week-3/Day-17-Top K Frequent Elements.cpp
@@ -0,0 +1,43 @@
+Problem Link :-https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3393/
+
+/*
+
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+You can return the answer in any order.
+
+*/
+
+//Solution
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int> ma;
+        vector<int> ans;
+        for(auto num:nums)
+         ma[num]++;
+        priority_queue<pair<int,int> > pa;
+        for(auto it:ma)
+            pa.push({it.second,it.first});
+        while(k--)
+        {
+            ans.push_back(pa.top().second);
+            pa.pop();
+        }
+        return ans;
+    }
+};
